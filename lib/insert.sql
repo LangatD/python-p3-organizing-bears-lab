c4c6562e1e8a6c0841d112142080860a7ac7f516
LangatD <dorcaschepngetich20@gmail.com>
2024-12-23 16:08:36 +0300

    SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr.Chocolate',2,'F','white','fddsafds',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',2,'F','white','fddsafds',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',2,'F','white','fddsafds',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',5,'M','white','fdfsfsdsafds',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',2,'M','blue','fddss',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',4,'F','white','fddsafds',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',5,'F','black','fddsafddds',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,2,'F','white','fddsafds',0);
+
